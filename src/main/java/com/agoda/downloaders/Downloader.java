a0e568003707f8bd3f00d4c854663b192bb5a03b
Worasit Daimongkol <worasit.dmk501@gmail.com>
2017-03-29 15:41:52 +0700

    Revised downloader to report download statuses.

--- src/main/java/com/agoda/downloaders/Downloader.java
+++ src/main/java/com/agoda/downloaders/Downloader.java
@@ -8,14 +8,32 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
+import java.text.MessageFormat;
+import java.util.concurrent.Callable;
+import java.util.logging.Logger;
 
-public abstract class Downloader implements Downloadable {
+public abstract class Downloader implements Downloadable, Callable<Boolean> {
 
     protected static final int START_BUFFER_POSITION = 0;
+    protected Logger logger = Logger.getLogger(Downloader.class.getSimpleName());
     private Source source;
 
     public Downloader(Source source) {
         this.source = source;
+    }
+
+
+    @Override
+    public Boolean call() throws Exception {
+        logger.info(MessageFormat.format("Starting download : {0}", this.getSource().getSourceURL()));
+        try {
+            download();
+        } catch (Exception e) {
+            logger.severe(MessageFormat.format("Failed to download : {0} error msg: {1}", this.getSource().getSourceURL(), e.getMessage()));
+            return false;
+        }
+        logger.info(MessageFormat.format("Finished download : {0}, the file is saved to {1}", this.getSource().getSourceURL(), this.getSource().getOutputFilePath()));
+        return true;
     }
 
     public void writeStreamData(InputStream inputStream, long contentLength) throws IOException {
